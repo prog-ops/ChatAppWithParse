1273c813eb802beec1d9f0cd8a101db33997c503
prgrm274 <programmer270487@gmail.com>
2020-07-11 11:04:40 +0700

    nyesuaikan dgn https://guides.codepath.com/android/Building-Data-driven-Apps-with-Parse

--- app/src/main/java/com/chat/prs/App.java
+++ app/src/main/java/com/chat/prs/App.java
@@ -5,17 +5,44 @@ import android.app.Application;
 import com.parse.Parse;
 import com.parse.Parse.Configuration;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+
 public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
 
+        //* https://guides.codepath.com/android/Building-Data-driven-Apps-with-Parse
+
+        // Use for troubleshooting -- remove this line for production
+        Parse.setLogLevel(Parse.LOG_LEVEL_DEBUG);
+        //* Use for monitoring Parse OkHttp traffic
+        // Can be Level.BASIC, Level.HEADERS, or Level.BODY
+        // See http://square.github.io/okhttp/3.x/logging-interceptor/ to see the options.
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        builder.networkInterceptors().add(httpLoggingInterceptor);
+        //* set applicationId, and server server based on the values in the Heroku settings.
+        // clientKey is not needed unless explicitly configured
+        // any network interceptors must be added with the Configuration Builder given this syntax
+        /*Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("myAppId") // should correspond to APP_ID env variable
+                .clientKey(null)  // set explicitly unless clientKey is explicitly configured on Parse server
+                .clientBuilder(builder)
+                .server("https://my-parse-app-url.herokuapp.com/parse/")
+                .build());
+*/
+
         // ngarang bisa
-        Parse.initialize(new Configuration
-                .Builder(this)
+        Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("9tvevK7kHRthCJFlyFFMZIkjL62s33ModYXpuz6K")
                 .clientKey("h275fYRTAXjQIjc9noqYWFcACXu1mi26qBxnUv8x")
-                .build());
+                .clientBuilder(builder)
+                .server("https://parseapi.back4app.com/")
+                .build()
+        );
 
         // tutorial, deprecated
         /*Parse.initialize(
